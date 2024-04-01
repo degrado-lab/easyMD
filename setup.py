b4277e4727ca300b2adbc44f464543ac7e471f69
njf042 <njf042@gmail.com>
2024-04-01 00:09:25 -0700

    Add setup.py file for easyMD package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='easyMD',
+    version='1.0.0',    
+    description='A set of utilities and notebooks for running OpenMM simulations.',
+    url='https://github.com/degrado-lab/easyMD',
+    author='Nicholas Freitas',
+    author_email='nicholas.freitas@ucsf.edu',
+    license='MIT',
+    packages=['easyMD'],
+    )
+
