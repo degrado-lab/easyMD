11e34df9508ac61e68be057836271e4f5be3ebb8
njf042 <njf042@gmail.com>
2025-03-22 23:35:48 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='easyMD',
-    version='3.0.0',    
+    version='3.1.0',    
     description='A set of utilities and notebooks for running OpenMM simulations.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
